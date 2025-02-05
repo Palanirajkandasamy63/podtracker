67fb84e72f82a6a9149c422d359fcf07a41612fa
palanirajkandasamy <palanirajkandasamy@gmail.com>
2025-02-04 21:42:51 -0600

    addingpodtrackertypes.go

--- api/v1/podtracker_types.go
+++ api/v1/podtracker_types.go
@@ -19,7 +19,11 @@ package v1
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
-
+type Reporter struct {
+	Kind    string `json:"kind,omitempty"`
+	Key     string `json:"key,omitempty"`
+	Channel string `json:"channel,omitempty"`
+}
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
@@ -27,9 +31,9 @@ import (
 type PodTrackerSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+    Name   string   `json:"name,omitempty"`
+	Report Reporter `json:"report,omitempty"`
 
-	// Foo is an example field of PodTracker. Edit podtracker_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
 }
 
 // PodTrackerStatus defines the observed state of PodTracker.
